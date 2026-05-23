d419692109a3014366cf093002232de1d0c14c35
Shahanaz Shaik <s.shaik1826@gmail.com>
2026-05-23 12:37:23 +0100

    Add transform stage - DataFrame and clean_data function

--- src/transform.py
+++ src/transform.py
@@ -1,10 +1,31 @@
 import json
+import pandas as pd
 
 try:
     with open('/home/shannu/Personal-Projects/uk-job-market-pipeline/data/raw_Data_analyst_20260520_193038.json', 'r') as file:
         data = json.load(file)
+        job_list = []
         for job in data["results"]:
-            print(job["title"], job["company"]["display_name"], job["location"]["display_name"], job["salary_min"], job["contract_type"])
-   
+            job_list.append({
+                "title" : job["title"],
+                "company" : job["company"]["display_name"],
+                "location" : job["location"]["display_name"],
+                "salary_min" : job["salary_min"],
+                "salary_max" : job["salary_max"],
+                "contract_type" : job["contract_type"]
+            })
+        df = pd.DataFrame(job_list)
+
 except FileNotFoundError:
     print("The file 'data.json' was not found.")
+
+def clean_data(title):
+    if "Trainee" in title:
+        return "Trainee Data Analyst"
+    if "No experience" in title:
+        return "Trainee Data Analyst"
+    else:
+        return title
+df["title"] = df["title"].apply(clean_data)
+
+print(df.head())
